f13b57161f3a42b4f938fdafa3e199557b6fc964
akcost <akselcosta@gmail.com>
2023-09-25 21:52:23 +0300

    Added save/find/delete result by id.

--- src/main/java/dao/ResultDao.java
+++ src/main/java/dao/ResultDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import model.Result;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +14,31 @@ public class ResultDao {
     @PersistenceContext
     private EntityManager em;
 
+    @Transactional
+    public Result save(Result result) {
+        if (result.getId() == null) {
+            em.persist(result);
+        } else {
+            em.merge(result);
+        }
+
+        return result;
+    }
+
     public List<Result> findAll() {
         return em.createQuery("select p from Result p").getResultList();
+    }
+
+    public Result findResultById(Long id) {
+        return em.find(Result.class, id);
+    }
+
+    @Transactional
+    public void deleteResultById(Long id) {
+        Result result = em.find(Result.class, id);
+
+        if (result != null) {
+            em.remove(result);
+        }
     }
 }
