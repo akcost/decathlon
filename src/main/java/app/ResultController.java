1de5916900aa47f318da1d3d3ada48c90869f3c4
akcost <akselcosta@gmail.com>
2023-09-25 21:58:18 +0300

    Added get results, get result by id, save result and delete result by id to ResultController.

--- src/main/java/app/ResultController.java
+++ src/main/java/app/ResultController.java
@@ -1,7 +1,9 @@
 package app;
 
 import dao.ResultDao;
+import jakarta.validation.Valid;
 import model.Result;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +22,23 @@ public class ResultController {
     @GetMapping("/results")
     public List<Result> getResults() {
         return dao.findAll();
+    }
+
+    @GetMapping("/results/{id}")
+    public Result getResultById(@PathVariable Long id) {
+        return dao.findResultById(id);
+    }
+
+    @PostMapping("/results")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Result save(@RequestBody @Valid Result result) {
+        return dao.save(result);
+    }
+
+    @DeleteMapping("/results/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteResultById(@PathVariable Long id) {
+        dao.deleteResultById(id);
     }
 
 }
